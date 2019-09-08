713b97d897ed2a995a2e31c0496b08de2175d76b
Robert Laws <roblaws@me.com>
2019-09-08 16:31:09 +0300

    add reactstrap for Bootstrap 4 features

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import { Container, Row, Col, Button } from 'reactstrap';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      React SPA
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor() {
+    super();
 
-export default App;
+    this.state = {
+      selection: '',
+      dropdownOpen: false
+    }
+  }
+
+  toggle = () => {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }))
+  }
+
+  handleClick = (event) => {
+    this.setState({
+      selection: event.target.name
+    })
+  }
+
+  render() {
+    return (
+      <Container className='App'>
+        <Row>
+          <Col>
+            <h1>React SPA</h1>
+            <p>{this.state.selection}</p>
+          </Col>
+          <Col>
+            <Button color="primary" size="lg">Click Here</Button>
+            <hr />
+            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+              <DropdownToggle color="primary" caret>
+                Make a Selection
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem name="Germany" onClick={this.handleClick}>Germany</DropdownItem>
+                <DropdownItem name="France" onClick={this.handleClick}>France</DropdownItem>
+                <DropdownItem name="Spain" onClick={this.handleClick}>Spain</DropdownItem>
+                <DropdownItem name="Italy" onClick={this.handleClick}>Italy</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
