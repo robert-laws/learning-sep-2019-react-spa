c6a9d039032277601d19a6993747b3ed4fde4a19
Robert Laws <roblaws@me.com>
2019-09-16 12:48:53 +0300

    add filter query to attendee list

--- src/components/attendees/attendees.component.jsx
+++ src/components/attendees/attendees.component.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import AttendeesList from '../attendeesList/attendeesList.component';
 import { withRouter } from 'react-router-dom';
 import { Container, Row, Col } from 'reactstrap';
+import { Card, CardBody } from 'reactstrap';
+import { Input, InputGroup, InputGroupAddon, Button } from 'reactstrap';
+import { FaUndo, FaRandom } from 'react-icons/fa';
 
 import firebase from '../../firebase/firebase';
 
@@ -13,7 +16,9 @@ class Attendees extends Component {
 
     this.state = {
       thisMeetingName: '',
-      displayAttendees: []
+      displayAttendees: [],
+      allAttendees: [],
+      searchQuery: ''
     }
   }
 
@@ -39,12 +44,40 @@ class Attendees extends Component {
       })
 
       this.setState({
+        allAttendees: attendeesList,
         displayAttendees: attendeesList
       })
     })
   }
 
+  handleChange = event => {
+    const itemName = event.target.name;
+    const itemValue = event.target.value;
+
+    this.setState({
+      [itemName]: itemValue
+    })
+  }
+
+  resetQuery = () => {
+    this.setState({
+      displayAttendees: this.state.allAttendees,
+      searchQuery: ''
+    })
+  }
+
+  chooseRandom = () => {
+    const randomAttendee = Math.floor(Math.random() * this.state.allAttendees.length);
+    this.resetQuery();
+    this.setState({
+      displayAttendees: [this.state.allAttendees[randomAttendee]]
+    })
+  }
+
   render() {
+    const dataFilter = item => item.attendeeName.toLowerCase().match(this.state.searchQuery.toLowerCase()) && true;
+    const filteredAttendees = this.state.displayAttendees.filter(dataFilter);
+
     return (
       <Container className='App attendees-section text-center' fluid>
         <Row className='justify-content-center'>
@@ -61,9 +94,28 @@ class Attendees extends Component {
             </h3>
           </Col>
         </Row>
+        <Row className='justify-content-center mb-4'>
+          <Col sm='4'>
+            <Card>
+              <CardBody>
+                <InputGroup>
+                  <Input type="text" name="searchQuery" value={this.state.searchQuery} placeholder="Search Attendees" onChange={this.handleChange} />
+                  <InputGroupAddon addonType="append">
+                    <Button color="primary" title="Pick a Random Attendee" onClick={this.chooseRandom}>
+                      <FaRandom />
+                    </Button>
+                    <Button color="primary" title="Reset Search" onClick={this.resetQuery}>
+                      <FaUndo />
+                    </Button>
+                  </InputGroupAddon>
+                </InputGroup>                
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
         <Row className='justify-content-center'>
           <Col sm='4'>
-            <AttendeesList userID={this.props.match.params.userID} meetingID={this.props.match.params.meetingID} attendees={this.state.displayAttendees} adminUser={this.props.adminUser} />
+            <AttendeesList userID={this.props.match.params.userID} meetingID={this.props.match.params.meetingID} attendees={filteredAttendees} adminUser={this.props.adminUser} />
           </Col>
         </Row>
       </Container>
